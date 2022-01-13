f2d9341f08006afbf587c169983b4f4e83b18964
thienv29 <thienv29@gmail.com>
2022-01-13 21:43:19 +0700

    first commit

--- .history/src/server_20220113214016.js
+++ .history/src/server_20220113214016.js
@@ -3,7 +3,7 @@ const getEvents = require("./google/google_calendar")
 const app = express()
 let CalendarFetch = []
 app.get("/", (req, res) => {
-    res.send('sync calendar')
+    res.send('')
 })
 app.listen(3000, () => {
     console.log('server is running');
